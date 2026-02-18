f22a3d295b417718c40115a0428c6660b7ac5fc3
Jayesh-008 <jayeshsubramani008@gmail.com>
2026-02-18 22:32:30 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (62.39%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
+
+
+        
+        
+
+
+        
